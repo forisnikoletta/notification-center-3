8e7ea50b5062292e191191d7ad06b32f61a97c6b
forisnikoletta <nky639@gmail.com>
2020-07-10 20:59:19 +0300

    Updated Notification controller and added Admin controller

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,7 +6,6 @@ module CurrentUserConcern
 	end
 
 	def set_current_user
-		puts "In currentUserConcern set_current_user"
 		if session[:user_id]
 			@current_user = User.find(session[:user_id])
 		end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :notification, only: [:create, :show]
+  resources :admin, only: [:create, :show, :update, :destroy]
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
 
   delete :logout, to: "sessions#logout"
-  
+
+  get :logged_in, to: "sessions#logged_in"
   get :logged_in, to: "sessions#logged_in"
 
-  root to: "notification#home"
+  get :admin, to: "admin#index"
+
+  root to: "notification#index"
 end
