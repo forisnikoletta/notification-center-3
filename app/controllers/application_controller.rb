045f356a190af7c64f2ba77b92d4cd548c61283f
forisnikoletta <nky639@gmail.com>
2020-07-10 17:44:58 +0300

    Added login,logout,registration and current_user concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+	# rails has a set of rules that will communicate with the route, one of them is csrf token 
+	# it checks to make sure that a user who's typing into a rails form is actually the real user.
+	# verify_authenticity_token will skip this
+	skip_before_action :verify_authenticity_token
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
-	 # this is gonna be a POST request, so when the front-end app will hit this api, it's going to make a post resuest here to create
-	 def create
+	# this is gonna be a POST request, so when the front-end app will hit this api, it's going to make a post resuest here to create
+	
+	include CurrentUserConcern
+
+	def create
 	 	#it will wrap up a user object
 	 	user = User
 	 			.find_by(email: params["user"]["email"])
@@ -18,5 +21,26 @@ class SessionsController < ApplicationController
 	 			status: 401
 	 		}
 	 	end
-	 end
+	end
+
+	def logged_in
+		if @current_user
+			render json: {
+				logged_in: true,
+				user: @current_user
+			}
+		else 
+			render json: {
+				logged_in: false
+			}
+		end
+	end
+
+	def logout
+		reset_session
+		render json: {
+			status: 200, 
+			logged_out: true
+		}
+	end
 end	 
