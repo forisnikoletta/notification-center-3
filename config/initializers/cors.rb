60e1853c2bad7f6ca3a9f3a55a720e6c655aa2a2
forisnikoletta <nky639@gmail.com>
2020-07-10 12:50:05 +0300

    Created and implemented cors and session store rb files

---
+++ config/initializers/cors.rb
@@ -0,0 +1,21 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+	# we are inserting a level of middleware here and we are going to use the cors to do it
+	# all of the rules what we place inside here are going to be intercepted by the rails configs
+
+	#if an app tries to communicate with our system that is not authorized to do so and they are coming from a domain which we have not whitelisted, we don't want to give them any acces
+
+	allow do
+		origin "http://localhost:3000"
+		resources "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+		# credentials: true will allow us to pass these headers back and forth and pass the cookie from the front-end to back-end app
+	end	
+
+
+	# it's a placeholder for the production app: we also have the domain that we are going to push this up to
+	# CONFIG ME
+	allow do
+		origin "https://some-name.something.com"
+		resources "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+		# credentials: true will allow us to pass these headers back and forth and pass the cookie from the front-end to back-end app
+	end
+end
