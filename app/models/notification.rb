93fea8c58b6a31e424e1a385927b52a3880e2cca
forisnikoletta <nky639@gmail.com>
2020-07-11 22:34:42 +0300

    Linked Notifications to User in the model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,8 @@
 class Notification < ApplicationRecord
+	validates_presence_of :title, :body
+
+	belongs_to :users
+
+	accepts_nested_attributes_for :users,
+								reject_if: lambda { |attrs| attrs['id'].blank? }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 	#it tells the User model that password_digest field needs to be encrypted
 	has_secure_password
 
+	has_many :notifications
+
 	validates_presence_of :email
 	validates_uniqueness_of :email
 end
